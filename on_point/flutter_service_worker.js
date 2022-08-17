0640e27a0e9f35732c3aad1055c42b79b8dac9f9
sjhorn <scott.horn@maersk.com>
2022-08-17 14:06:22 +0200

    add fonts

--- on_point/flutter_service_worker.js
+++ on_point/flutter_service_worker.js
@@ -4,10 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "dcdb668ab0550ec94a10370bfdaf439f",
-"index.html": "b3dde5f1d7b4bc86b2e6a2ea6bd19021",
-"/": "b3dde5f1d7b4bc86b2e6a2ea6bd19021",
+"index.html": "68fee9393838a456cc584ffe58a50953",
+"/": "68fee9393838a456cc584ffe58a50953",
 "main.dart.js": "ffcfe3d46c316370f0807d2097a3b2e1",
-"flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -15,14 +14,10 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "8d98bb9a9a10edbe1085dd5dd98dc65e",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "fb1c75d99c3b75b6e74457f66aeb0327",
+"assets/NOTICES": "d4eaaac66ed195cdc8d2a1755d562bc1",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
-"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
-"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
-"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba"
+"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b"
 };
 
 // The application shell files that are downloaded before a service worker can
